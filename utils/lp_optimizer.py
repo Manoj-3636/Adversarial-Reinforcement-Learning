eae5f7acf73032eb26f92b7084a3a125e322d9f1
Manoj Kumar <kvssmk36@gmail.com>
2026-04-26 16:21:16 +0530

    feat: modified lp_optimizer to use np arrays

--- utils/lp_optimizer.py
+++ utils/lp_optimizer.py
@@ -1,11 +1,13 @@
 import nashpy as nash
+import numpy as np
 
-def compute_nash_equilibrium(utility_matrix):
+def compute_nash_equilibrium(U):
 
-    game = nash.Game(utility_matrix)
+    game = nash.Game(U, -U)
+
     equilibria = game.support_enumeration()
 
-    for eq in equilibria :
-        sigma_def, sigma_att = eq
-        return sigma_def, sigma_att
+    for sigma_def, sigma_att in equilibria:
+        return np.array(sigma_def), np.array(sigma_att)
 
+    raise ValueError("No equilibrium found")
